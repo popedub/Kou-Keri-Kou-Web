5124428912ab8d9f6ab4f9346a593423f241c82c
popedub <pablo.volt@gmail.com>
2018-04-10 09:55:06 +0200

    segundo deploy, manual, al server de desarrollo

--- web/app/themes/koutheme/resources/views/partials/content-single.blade.php
+++ web/app/themes/koutheme/resources/views/partials/content-single.blade.php
@@ -1,13 +1,62 @@
 <article @php(post_class())>
   <header>
     <h1 class="entry-title">{{ get_the_title() }}</h1>
-    @include('partials/entry-meta')
   </header>
   <div class="entry-content">
+    {{-- si es un disco --}}
+    @if(in_category('discos'))
+      @if(get_field('bandcamp') )
+        @php(the_field('bandcamp'))
+      @endif
+    @endif
+
+    {{-- si es un concierto --}}
+    @if(in_category('conciertos'))
+      @if(has_post_thumbnail())
+        @php(the_post_thumbnail('destacado', array('class' => 'img-fluid')))
+      @elseif(get_field('embeds'))
+        <div class="embed-responsive embed-responsive-16by9">
+          @php(the_field('embeds'))
+        </div>
+      @elseif(get_field('videos'))
+        <div class="embed-responsive embed-responsive-16by9">
+          @php(the_field('videos'))
+        </div>
+      @endif
+
+      @if(get_field('info') )
+        @php(the_field('info'))
+      @endif
+    @endif
+
+    {{-- si es un video --}}
+    @if(in_category('videos'))
+      @if(get_field('video'))
+        <div class="embed-responsive embed-responsive-16by9">
+          @php(the_field('video'))
+        </div>
+        @php(the_content())
+      @endif
+    @endif
+
+    {{-- si es una noticia --}}
+    @if(in_category('noticias'))
+      @if(get_field('embeds'))
+        <div class="embed-responsive embed-responsive-16by9">
+          @php(the_field('embeds'))
+        </div>
+      @elseif(get_field('videos'))
+        <div class="embed-responsive embed-responsive-16by9">
+          @php(the_field('videos'))
+        </div>
+      @elseif(has_post_thumbnail())
+        @php(the_post_thumbnail('destacado', array('class' => 'img-fluid')))
+      @endif
+
     @php(the_content())
+    @endif
   </div>
   <footer>
     {!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']) !!}
   </footer>
-  @php(comments_template('/partials/comments.blade.php'))
 </article>
